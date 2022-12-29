2a3715aa80a8943af9c97a82d3fef269abc54991
Aleksandr Cheverda <alexar@ua.fm>
2022-12-29 21:49:40 +0200

    Edit filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,27 +1,37 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { filterContact } from 'redux/filter/filterActions';
 // import PropTypes from 'prop-types';
-import { FilterBox, Label, Input } from './Filter.Styled';
+// import { FilterBox, Label, Input } from './Filter.Styled';
+import { Form, FloatingLabel } from 'react-bootstrap';
+import { getFilter } from 'redux/contacts/contactsSelector';
+import style from './Filter.module.css';
 
 const Filter = () => {
-    const filter = useSelector(state => state.filter);
+    const filter = useSelector(getFilter);
     const dispatch = useDispatch();
     const changeFilter = evt => {
         dispatch(filterContact(evt.currentTarget.value));
     };
     return (
-        <FilterBox>
-            <Label>Find contacts by name</Label>
-            <Input
-                type="text"
-                name="filter"
-                value={filter}
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                onChange={changeFilter}
-            />
-        </FilterBox>
+        <Form className={style.filter}>
+            <Form.Group controlId="ControlInputFilter">
+                <FloatingLabel
+                    controlId="ControlInputFilter"
+                    label="Find contact by name"
+                >
+                    <Form.Control
+                        type="text"
+                        name="filter"
+                        value={filter}
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        onChange={changeFilter}
+                        className={style.filterInput}
+                    />
+                </FloatingLabel>
+            </Form.Group>
+        </Form>
     );
 };
 
